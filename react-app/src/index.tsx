72745aee4cd0aa5b2f999e102f99c0d76d9f5d2c
Yasunori Takahashi <hibriiiiidge@gmail.com>
2022-09-10 09:54:53 +0900

    Add Name components

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
+import Name from './components/Name';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Name />
   </React.StrictMode>
 );
 
