b006fbc5efab53739167dcf3aa91c066d93301a3
Yasunori Takahashi <hibriiiiidge@gmail.com>
2022-09-18 10:12:43 +0900

    Add Provider/Consumer

--- react-app/src/components/ContainerSample.tsx
+++ react-app/src/components/ContainerSample.tsx
@@ -1,29 +1,62 @@
 import React from "react";
 
-type ContainerProps = {
-  title: string,
-  children: React.ReactNode
+const TitleContext = React.createContext('')
+
+const Title = () => {
+  return (
+    <TitleContext.Consumer>
+      {
+        (title) => {
+          return <h1>{title}</h1>
+        }
+      }
+    </TitleContext.Consumer>
+  )
 }
 
-const Container = (props: ContainerProps): JSX.Element => {
-  const { title, children } = props
-
+const Header = () => {
   return (
-    <div style={{ background: 'red' }}>
-      <span>{title}</span>
-      <div>{children}</div>
+    <div>
+      <Title />
     </div>
   )
 }
 
+const Page = () => {
+  const title = 'React Book'
 
-const Parent = (): JSX.Element => {
   return (
-    <Container title='Hello'>
-      <p>ここの部分が背景色で囲まれます</p>
-    </Container>
+    <TitleContext.Provider value={title}>
+      <Header />
+    </TitleContext.Provider>
   )
 }
 
 
-export default Parent
+// type ContainerProps = {
+//   title: string,
+//   children: React.ReactNode
+// }
+
+// const Container = (props: ContainerProps): JSX.Element => {
+//   const { title, children } = props
+
+//   return (
+//     <div style={{ background: 'red' }}>
+//       <span>{title}</span>
+//       <div>{children}</div>
+//     </div>
+//   )
+// }
+
+
+// const Parent = (): JSX.Element => {
+//   return (
+//     <Container title='Hello'>
+//       <p>ここの部分が背景色で囲まれます</p>
+//     </Container>
+//   )
+// }
+
+
+export default Page
