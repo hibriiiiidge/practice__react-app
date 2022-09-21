a9aa26fdb5edfc3f21c0647d4b9fd429c57e8a13
Yasunori Takahashi <hibriiiiidge@gmail.com>
2022-09-21 23:02:19 +0900

    Refactor UseEffectSample

--- react-app/src/components/UseCallbackSample.tsx
+++ react-app/src/components/UseCallbackSample.tsx
@@ -1,51 +1,73 @@
-import React, { useState, useCallback } from "react";
+import { useState, useEffect } from 'react'
 
-type ButtonProps = {
-  onClick: () => void
+// タイマーが呼び出される周期を1秒にする
+const UPDATE_CYCLE = 1000
+
+// localstorageで使用するキー
+const KEY_LOCALE = 'KEY_LOCALE'
+
+enum Locale {
+  US = 'en-US',
+  JP = 'ja-JP',
 }
 
-const DecrementButton = (props: ButtonProps) => {
-  const { onClick } = props
-  console.log(`DecrementButtonが再描画されました`);
-
-  return <button onClick={onClick}>Decrement</button>
+const getLocaleFromString = (text: string) => {
+  switch (text) {
+    case Locale.US:
+      return Locale.US
+    case Locale.JP:
+      return Locale.JP
+    default:
+      return Locale.US
+  }
 }
 
-const IncrementButton = React.memo((props: ButtonProps) => {
-  const { onClick } = props
-  console.log(`IncrementButtonが再描画されました`);
+const Clock = () => {
+  const [timestamp, setTimestamp] = useState(new Date())
+  const [locale, setLocale] = useState(Locale.US)
 
-  return <button onClick={onClick}>Increment</button>
-})
+  // タイマーのセットをするための副作用
+  useEffect(() => {
+    // タイマーのセット
+    const timer = setInterval(() => {
+      setTimestamp(new Date())
+    }, UPDATE_CYCLE)
 
-const DoubleButton = React.memo((props: ButtonProps) => {
-  const { onClick } = props
-  console.log(`DoubleButtonが再描画されました`);
-
-  return <button onClick={onClick}>Double</button>
-})
-
-const Parent = () => {
-  const [count, setCount] = useState(0)
-  const decrement = () => {
-    setCount((c) => c-1)
-  }
-  const increment = () => {
-    setCount((c) => c+1)
-  }
-  const double = useCallback(() => {
-    setCount((c) => c*2)
+    // クリーンアップ関数を渡し、アンマウント時にタイマーの解除をする
+    return () => {
+      clearInterval(timer)
+    }
+    // 初期描画時のみ実行する
   }, [])
+
+  // localstorageから値を読み込むための副作用
+  useEffect(() => {
+    const savedLocale = localStorage.getItem(KEY_LOCALE)
+    if (savedLocale !== null) {
+      setLocale(getLocaleFromString(savedLocale))
+    }
+  }, [])
+
+  // localeが変化した時に、localstorageに値を保存するための副作用
+  useEffect(() => {
+    localStorage.setItem(KEY_LOCALE, locale)
+    // 依存配列にlocaleを渡し、localeが変化する度に実行するようにする
+  }, [locale])
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <DecrementButton onClick={decrement}/>
-      <IncrementButton onClick={increment}/>
-      <DoubleButton onClick={double} />
+      <p>
+        <span id="current-time-label">現在時刻</span>
+        <span>:{timestamp.toLocaleString(locale)}</span>
+        <select
+          value={locale}
+          onChange={(e) => setLocale(getLocaleFromString(e.target.value))}>
+          <option value="en-US">en-US</option>
+          <option value="ja-JP">ja-JP</option>
+        </select>
+      </p>
     </div>
   )
 }
 
-
-export default Parent
+export default Clock
--- react-app/src/components/UseEffectSample.tsx
+++ react-app/src/components/UseEffectSample.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 const UPDATE_CYCLE = 1000
 
@@ -6,10 +6,10 @@ const KEY_LOCALE = 'KEY_LOCALE'
 
 enum Locale {
   US = 'en-US',
-  JP = 'js-JP'
+  JP = 'ja-JP'
 }
 
-const getLocalFromString = (text: string) => {
+const getLocaleFromString = (text: string) => {
   switch (text) {
     case Locale.US:
       return Locale.US
@@ -29,7 +29,7 @@ const Clock = () => {
       setTimestamp(new Date())
     }, UPDATE_CYCLE)
 
-    return () =>{
+    return () => {
       clearInterval(timer)
     }
   }, [])
@@ -37,7 +37,7 @@ const Clock = () => {
   useEffect(() => {
     const savedLocale = localStorage.getItem(KEY_LOCALE)
     if(savedLocale !== null) {
-      setLocale(getLocalFromString(savedLocale))
+      setLocale(getLocaleFromString(savedLocale))
     }
   }, [])
 
@@ -45,14 +45,15 @@ const Clock = () => {
     localStorage.setItem(KEY_LOCALE, locale)
   }, [locale])
 
+
   return (
     <div>
       <p>
         <span id="current-time-label">現在時刻</span>
-        <span>: { timestamp.toLocaleString(locale) }</span>
+        <span>:{timestamp.toLocaleString(locale)}</span>
         <select
           value={locale}
-          onChange={(e) => setLocale(getLocalFromString(e.target.value))}>
+          onChange={(e) => setLocale(getLocaleFromString(e.target.value))}>
           <option value="en-US">en-US</option>
           <option value="ja-JP">ja-JP</option>
         </select>
